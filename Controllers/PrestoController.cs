6e228259a04f55fc6ceee0890dd99d0e9c899463
Ben Sainsbury <redtrails@gmail.com>
2015-07-22 13:30:23 -0700

    Initial commit

---
+++ Controllers/PrestoController.cs
@@ -0,0 +1,52 @@
+﻿
+using System.Web.Mvc;
+using System;
+using System.Linq;
+using System.Data.Spatial;
+using Enyim.Collections;
+
+namespace Presto.Controllers
+{
+  public class PrestoController : Controller
+  {
+    [AcceptVerbs(HttpVerbs.Get)]
+    public ActionResult Status(string service)
+    {
+        return Json(new {taxlotCount=Global.taxlots.Count()}, "application/json", JsonRequestBehavior.AllowGet);
+    }
+
+    [AcceptVerbs(HttpVerbs.Get)]
+    public ActionResult Identify(string service)
+    {
+      double x, y;
+      var is_x = Double.TryParse(Request.Params["x"], out x);
+      var is_y = Double.TryParse(Request.Params["y"], out y);
+
+      if (!is_x || !is_y)
+      {
+        return Json(new { error = true, message = "Please provide an x and y value in decimal degrees" }, JsonRequestBehavior.AllowGet);
+      }
+
+      var inPoint = DbGeometry.FromText("POINT("+x+" "+y+")", 4326);
+      var env = new Envelope(x, y, x, y);
+
+      var t = Global.r.Search(env);
+      var res = t.Where(p => Global.taxlots[p.Data].Intersects(inPoint));
+      var test = res.Count();
+
+      if (test <= 0) return Json(new {x, y, error = true, message = "Nobody there"}, JsonRequestBehavior.AllowGet);
+
+      var taxlotnum = res.FirstOrDefault().Data;
+
+      return Json(new {TLID = taxlotnum, geom = Global.taxlots[taxlotnum].AsText()}, "application/json", JsonRequestBehavior.AllowGet);
+    }
+
+    [AcceptVerbs(HttpVerbs.Get)]
+    public ActionResult Test()
+    {
+      var foo = DbGeometry.FromText(
+        "POLYGON ((-122.66018778173442 45.601605211473235,-122.66024700952981 45.6014165334974,-122.6603231949731 45.601428354870642,-122.6602639784374 45.60161701917621,-122.66018778173442 45.601605211473235))");
+      return Json(new { foo }, JsonRequestBehavior.AllowGet);
+    }
+  }
+}
---
+++ Global.asax.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Spatial;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Enyim.Collections;
+
+namespace Presto
+{
+
+    public class Global : HttpApplication
+    {
+
+      internal static Dictionary<string, DbGeometry> taxlots;
+      internal static RTree<string> r;
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+               "Status", // Route name
+               "status/{service}", // URL with parameters
+               new { controller = "Presto", action = "Status" } // Parameter defaults
+           );
+
+            routes.MapRoute(
+                 "Identify", // Route name
+                 "identify/{service}", // URL with parameters
+                 new { controller = "Presto", action = "Identify" } // Parameter defaults
+             );
+
+            routes.MapRoute(
+                 "Test", // Route name
+                 "test/", // URL with parameters
+                 new { controller = "Presto", action = "Test" } // Parameter defaults
+             );
+        }
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            RegisterRoutes(RouteTable.Routes);
+
+            taxlots = new Dictionary<string, DbGeometry>();
+
+            var lines = System.IO.File.ReadAllLines(@"c:/temp/tracts2.csv");
+
+            r = new RTree<string>();
+
+            foreach (var line in lines)
+            {
+
+              if (line.IndexOf('"') == -1) continue;
+
+              var str_geom = line.Substring(1, line.IndexOf('"', 2)).Replace("\"", "");
+              var TLID = "";
+              var _geom = DbGeometry.FromText(str_geom, 4326);
+              var _attr = line.Substring(line.IndexOf('"', 2), line.Length - line.IndexOf('"', 2));
+              try
+              {
+                TLID = _attr.Replace("\",", "");
+                Global.taxlots.Add(TLID, _geom);
+                //attr.Add(Convert.ToInt32(DissolveID), _attr.Replace(DissolveID, "").Replace(",","").Replace("\"",""));
+                var _boundary = _geom.Envelope;
+
+                var env = new Envelope((double)_boundary.PointAt(1).XCoordinate, (double)_boundary.PointAt(1).YCoordinate,
+                  (double)_boundary.PointAt(3).XCoordinate, (double)_boundary.PointAt(3).YCoordinate);
+
+                r.Insert(TLID, env);
+              }
+              catch (Exception ex)
+              {
+              }
+            }
+        }
+    }
+}
